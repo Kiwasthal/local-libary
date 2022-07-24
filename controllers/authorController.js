e81306bdcd66f84087f38fd56277d32061c920c8
Steerpike <imthekoul@gmail.com>
2022-07-24 14:20:27 +0300

    Update catalog route and index

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -8,7 +8,7 @@ exports.author_list = (req, res) => {
 
 //Display detail page for a specific Author.
 
-exports.author_detaul = (req, res) => {
+exports.author_detail = (req, res) => {
   res.send('NOT IMPLEMENTED : Author detail: ' + req.params.id);
 };
 
@@ -29,7 +29,21 @@ exports.author_delete_get = (req, res) => {
   res.send('NOT IMPLEMENTED : Author delete GET');
 };
 
+// Handle Author delete on POST.
+exports.author_delete_post = function (req, res) {
+  res.send('NOT IMPLEMENTED: Author delete POST');
+};
+
+// Display Author update form on GET.
+exports.author_update_get = function (req, res) {
+  res.send('NOT IMPLEMENTED: Author update GET');
+};
+
 //Handle Author update on POST
 exports.author_upadte_post = (req, res) => {
   res.send('NOT IMPLEMENTED: Authro update POST');
 };
+
+//The module first requries the model that we'll later  be using to access and update our data. It then exports function for each of the URLs we wish to handle (the create, update and delete operations use forms, and hence also have additional methods for handling form post requests - we'll discuss those methods in the 'forms article' later on).
+
+//All the functions have the standard form of an Express middleware function, with arguments for the request and response. We could also include the next function to be called if the method does not complete the request cycle, but in al lthese cases it does, so we've omitted it. The methods return a string indicating that the associated page has not yet been created. If a controller function is exprected to receive path parameters, these are output in the message string ( see req.params.id above)
